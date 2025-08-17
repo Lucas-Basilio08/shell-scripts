fb71b2db1e108ef05df8a8e65224e1e018840f94
Lucas <lucasbasiliorf@gmail.com>
2025-08-16 22:11:51 -0300

    Adicionei uma modificação do exercicio 1 com uma variavel de erro adicional na pasta 02

--- 02-variaveis-e-entrada.sh
+++ 02-variaveis-e-entrada.sh
@@ -18,6 +18,27 @@ IDADE=$(echo "$ANO_ATUAL-$ANO_NASC" | bc -l)
 # Imprimir na tela
 echo "Olá, $NOME"
 echo "Esse ano você tem/terá $IDADE anos"
+#############################################################################
 
+
+
+## Exercicio 2: modificar o calculo de idade para quando o usuario não colocar um nome ou uma data, a variavél retorne com alguma mensagem de erro.
+
+# Pedir informações
+read -p "Digite seu nome: " NOME
+read -p "Digite o ano em que nasceu: " ANO_NASC
+
+# Modificação (mensagem de erro)
+echo "Nome Cadastrado: ${NOME:?Usuario não forneceu o nome\!}"
+echo "Data Nasc: ${ANO_NASC:?Usuario não forneceu o ano de nascimento\!}"
+
+# Calcular idade 
+ANO_ATUAL=$(date +%Y)
+IDADE=$(echo "$ANO_ATUAL-$ANO_NASC" | bc -l)
+
+# Imprimir na tela
+echo "Olá, $NOME"
+echo "Esse ano você tem/terá $IDADE anos"
+#############################################################################
 
 
